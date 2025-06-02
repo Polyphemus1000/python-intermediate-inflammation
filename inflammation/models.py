36527436fc4e9a2d8941cf0d021f632f867ddb6b
Polyphemus1000 <152412788+Polyphemus1000@users.noreply.github.com>
2025-06-02 16:18:50 +0100

    Hello

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,26 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
+    """Calculate the daily mean of a 2d inflammation data array.
+    : param data : 2d Data file in CSV format 
+    ; returns 1d data of mean calculated along the columns
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array."""
+    """Calculate the daily max of a 2d inflammation data array.
+    : param data : 2d Data file in CSV format 
+    ; returns 1d data of max values calculated along the columns
+    
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array."""
+    """Calculate the daily min of a 2d inflammation data array.
+    : param data : 2d Data file in CSV format 
+    ; returns 1d data of min values calculated along the columns
+    """
     return np.min(data, axis=0)
 
